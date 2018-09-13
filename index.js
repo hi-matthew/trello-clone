fb0a885cc4fcf677016714beb926903a157a9f7b
Matthew Martinez <hellomattmartinez@gmail.com>
2018-09-13 16:37:13 -0700

    initialize repo / trello clone mvp

--- index.js
+++ index.js
@@ -1,28 +1,164 @@
 const boardName = document.querySelector('.board-name');
-const renamePopUp = document.querySelector('.rename-board');
-const closeIcon = document.querySelector('.fas');
+const renamePopUp = document.querySelector('.rename-board.wrapper');
+const boardCloseIcon = document.querySelector('.close-board');
+const listCloseIcon = document.querySelector('.close-list');
 const renameBoardForm = document.querySelector('form');
+const renameTextField = document.querySelector('.rename-board input[type=text]');
+const listTextField = document.querySelector('.create-list input[type=text]');
+const canvas = document.querySelector('.canvas');
+const addListButton = document.querySelector('.add-list');
+const createList = document.querySelector('.create-list');
+const createListForm = document.querySelector('.create-list form');
 
 // Retrieves Board Name from Local Storage
 localStorage.getItem('boardName') ? boardName.innerHTML = localStorage.getItem('boardName') : '';
 
+// Opens rename board pop up
 function openRenameBoard() {
   renamePopUp.classList.add('open');
+  renameTextField.focus();
 }
 
+// Closes rename board pop up
 function closeRenameBoard() {
-  renamePopUp.classList.remove('open');
-}
-
-function renameBoard(e) {
-  e.preventDefault();
-  let value = document.querySelector('input[type=text]').value;
-  boardName.innerHTML = value;
-  localStorage.setItem('boardName', value);
   renamePopUp.classList.remove('open');
   renameBoardForm.reset();
 }
+ 
+// Renames board with user's input value
+function renameBoard(e) {
+  e.preventDefault();
+  let value = renameTextField.value.trim();
+
+  if(value.length < 1) {
+    renameBoardForm.reset();
+  } else {
+    boardName.innerHTML = value;
+    localStorage.setItem('boardName', value);
+    renamePopUp.style.top = '-50%';
+  }
+}
+
+// Opens the "Add a list" modal
+function addListPopUp() {
+  const addListCoords = {
+    'top': document.querySelector('.add-list-button').getBoundingClientRect().top,
+    'left': document.querySelector('.add-list-button').getBoundingClientRect().left
+  }
+
+  createList.style.top = `${addListCoords.top}px`;
+  createList.style.left = `${addListCoords.left}px`;
+
+  createList.classList.add('open');
+  listTextField.focus();
+}
+
+// Closes the "Add a list" modal
+function closeList() {
+  createList.classList.remove('open');
+  createListForm.reset();
+}
+
+// Creates a new list column with user's input as the title
+function createListColumn(e) {
+  e.preventDefault();
+  let value = listTextField.value.trim()
+  
+  if (value.length < 1) {
+    createListForm.reset()
+  } else {
+  const createdList = document.createElement('div');
+  createdList.classList.add('list-column');
+  canvas.insertBefore(createdList, document.querySelector('.add-list-button'));
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+  createdList.appendChild(card);
+
+  const cardHeading = document.createElement('div');
+  cardHeading.classList.add('card-heading');
+  card.appendChild(cardHeading);
+
+  const cardTitle = document.createElement('p');
+  cardTitle.classList.add('card-title');
+  cardHeading.appendChild(cardTitle);
+  cardTitle.innerHTML = value;
+
+  const cardHeadingOptions = document.createElement('span');
+  cardHeadingOptions.classList.add('card-options');
+  cardHeading.appendChild(cardHeadingOptions);
+  cardHeadingOptions.innerHTML = '<i class="fas fa-ellipsis-h"></i>';
+
+  const cardContent = document.createElement('div');
+  cardContent.classList.add('card-content');
+  card.appendChild(cardContent);
+
+  const addCardDetails = document.createElement('a');
+  addCardDetails.classList.add('add-card-details');
+  card.appendChild(addCardDetails);
+  addCardDetails.innerHTML = '<span class="card-details-span">+ Add a card</span>';
+
+  const cardDetailsForm = document.createElement('form');
+  addCardDetails.appendChild(cardDetailsForm);
+  cardDetailsForm.innerHTML = 
+    `<textarea rows='4' columns='4' autofocus></textarea>
+      <div class='submit-options'>
+        <span class="invisibile"></span>
+        <input type='submit' value = 'Add card' class='add-card'>
+        <i class="fas fa-times close-details"></i>
+      </div>`
+
+  addListButton.innerHTML === '+ Add a list' ? addListButton.innerHTML = '+ Add another list' : '';
+  closeList();
+  createListForm.reset();
+  addListPopUp();
+  }
+}
+
+document.body.addEventListener('click', (e) => {
+  let tar = e.target;
+  console.log(tar)
+
+  if (tar.className === 'card-details-span') {
+    tar.parentNode.classList.add('open');
+    console.log(tar.parentNode.previousSibling);
+    tar.nextSibling.firstElementChild.focus();
+  }
+
+  tar.className === 'fas fa-times close-details' ? tar.parentNode.parentNode.parentNode.classList.remove('open') : '';
+
+  if (tar.className === 'add-card') {
+    e.preventDefault();
+    const textarea = tar.parentNode.parentNode.firstElementChild;
+    
+    const newCard = document.createElement('span');
+    if(textarea.value.trim().length < 1) {
+      textarea.parentNode.reset();
+      textarea.focus();
+    } else {
+      newCard.innerHTML = textarea.value;
+      textarea.parentNode.reset();
+      const cardContent = tar.parentNode.parentNode.parentNode.previousSibling;
+      cardContent.appendChild(newCard);
+      textarea.focus();
+    }  
+  }
+});
 
 boardName.addEventListener('click', openRenameBoard);
-closeIcon.addEventListener('click', closeRenameBoard);
+boardCloseIcon.addEventListener('click', closeRenameBoard);
 renameBoardForm.addEventListener('submit', renameBoard);
+renamePopUp.addEventListener('transitionend', () => {
+  renamePopUp.classList.remove('open');
+  renamePopUp.style.top = '75px';
+  renameBoardForm.reset();
+});
+addListButton.addEventListener('click', addListPopUp);
+listCloseIcon.addEventListener('click', closeList);
+createList.addEventListener('submit', createListColumn);
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 27) {
+    createList.className.includes('open') ? closeList() : '';
+    renamePopUp.className.includes('open') ? closeRenameBoard() : '';
+  }
+});
